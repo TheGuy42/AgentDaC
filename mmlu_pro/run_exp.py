e29e21a8d5d26ab981bce485afe3db92ef551bf0
TheGuy42 <guycohen801@gmail.com>
2025-07-06 20:13:50 +0300

    added MMLU_PRO dataset!

---
+++ mmlu_pro/run_exp.py
@@ -0,0 +1,107 @@
+# import unsloth
+from art.dev.model import InitArgs, EngineArgs, PeftArgs, TrainerArgs, InternalModelConfig
+import argparse
+import os
+import art
+
+from trainer import MMLU_ProTrainer
+from art_model_config import configs
+
+def parse_args():
+    """
+    Parse command line arguments.
+    """
+    parser = argparse.ArgumentParser(description="Run the Easy2Hard training process.")
+    
+    parser.add_argument(
+        "--model_name",
+        type=str,
+        help="The name of the model to use for training.",
+        required=True,
+    )
+    # parser.add_argument(
+    #     "--model_config",
+    #     type=str,
+    #     default="32B",
+    #     help="The name of the model configuration to use."
+    # )
+    parser.add_argument(
+        "--epochs",
+        type=int,
+        default=3,
+        help="Number of epochs to train the model."
+    )
+    parser.add_argument(
+        "--n_rollouts",
+        type=int,
+        default=10,
+        help="Number of rollouts per group."
+    )
+    parser.add_argument(
+        "--n_groups",
+        type=int,
+        default=5,
+        help="Number of groups to use for training."
+    )
+    parser.add_argument(
+        "--gpu",
+        type=int,
+        default=0,
+        help="GPU ID to use for training."
+    )
+    parser.add_argument(
+        "--vllm_server_ports",
+        type=int,
+        nargs='+',
+        default=[],  # Default port for vLLM server
+        help="List of ports for the vLLM servers."
+    )
+    parser.add_argument(
+        "--run_name",
+        type=str,
+        default=None,
+        help="Name of the run. If provided, it will be used to resume the training from the given run name."
+    )
+    
+    args = parser.parse_args()
+    return args
+
+
+async def main():
+    """
+    Main function to run the training process.
+    """
+    WANDB_API_KEY = "308e1be7938bf7a7c366afc0522fb9fc0d8cf1ad"
+    args = parse_args()
+    print("Running training with the following arguments:")
+    print(args)
+    
+    model_config:InternalModelConfig = configs.get(args.model_name, None)
+
+    # Initialize the trainer
+    trainer = MMLU_ProTrainer(
+        model_name=args.model_name,
+        model_config=model_config,
+        run_name=args.run_name,
+        WANDB_API_KEY=WANDB_API_KEY,
+        seed=42,
+        gpu=args.gpu,
+        vllm_server_ports=args.vllm_server_ports
+    )
+
+    # Load the model
+    await trainer.load_model()  # Example port, adjust as necessary
+
+    # Start training
+    await trainer.train(
+        epochs=args.epochs,
+        n_rollouts=args.n_rollouts,
+        n_groups=args.n_groups,
+        )
+
+if __name__ == "__main__":
+    import asyncio
+    asyncio.run(main())
+    # Note: Ensure that the vLLM servers are running on the specified ports before starting the training process.
+    # You can start the vLLM servers using the `run_vllm.server.py` script with the appropriate model configurations.
+    
---
+++ mmlu_pro/trainer.py
@@ -0,0 +1,161 @@
+import torch
+import numpy as np
+import os
+import sys
+from dotenv import load_dotenv
+import random
+import re
+from datasets import Dataset, load_dataset
+import regex
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import art
+from art import Trajectory
+from art.dev.model import InternalModelConfig
+from art.local import LocalBackend
+import openai
+from openai import AsyncOpenAI
+
+from training import Trainer
+from sys_prompt import SystemPrompt, DaCSystemPrompt
+from dac_agent import DACAgent, extract_text_between_markers
+from vllm_client import VllmClient, VllmRouter
+
+
+class MMLU_ProTrainer(Trainer):
+    def __init__(
+        self,
+        model_name: str,
+        model_config: InternalModelConfig,
+        project_name: str = "mmlu-pro-dac_agent",
+        run_name: str | None = None,
+        backend: LocalBackend = LocalBackend(path="./.art"),
+        WANDB_API_KEY: str = "",
+        OPENPIPE_API_KEY: str = "",
+        seed: int = 42,
+        gpu: int = 0,
+        vllm_server_ports: list[int] = []
+    ):
+        super().__init__(
+            model_name=model_name,
+            model_config=model_config,
+            project_name=project_name,
+            run_name=run_name,
+            backend=backend,
+            WANDB_API_KEY=WANDB_API_KEY,
+            OPENPIPE_API_KEY=OPENPIPE_API_KEY,
+            seed=seed,
+            gpu=gpu,
+            vllm_server_ports=vllm_server_ports
+        )
+
+        print(f"Easy2HardTrainer:: Loading the dataset..")
+        self.dataset = load_dataset("TIGER-Lab/MMLU-Pro")
+        self.dataset = self.dataset.map(self.generate_question_with_options)
+
+        # The dataset is usually split into 'train' and 'test'
+        self.train_data:Dataset = self.dataset['test']
+        self.test_data:Dataset = self.dataset['validation']
+        
+    @staticmethod
+    def generate_question_with_options(sample):
+        question = sample["question"]
+        options = sample["options"]
+        options_str = "\n".join([f"{chr(65 + i)}. {opt}" for i, opt in enumerate(options)])
+        sample['question_with_options'] = f"{question}\n{options_str}"
+        return sample
+
+    async def _train(self, epoch, n_rollouts, n_groups, vllm_router):
+        # Split the dataset into n_groups
+        # Each group will have n_rollouts samples
+        # each group will have a greater difficulty level
+        epoch_data_groups:list[Dataset] = []
+        epoch_data = self.train_data.shuffle(seed=42)
+        epoch_data = epoch_data.take(n_rollouts*n_groups)  # Take the first n_rollouts samples for training
+        epoch_data = epoch_data.sort("item_difficulty", reverse=False)
+        for i in range(n_groups):
+            epoch_data_groups.append(epoch_data.shard(num_shards=n_groups, index=i, contiguous=True))
+
+        train_groups = await art.gather_trajectory_groups(
+            (
+                art.TrajectoryGroup(
+                    rollout(
+                        vllm_client=vllm_router.__next__(),
+                        question=sample['question_with_options'][0],
+                        answer=sample['answer'][0], 
+                        model_config=self.model_config,
+                    ) for i, sample in enumerate(epoch_data.iter(batch_size=1)) # Number of rollouts per group
+                ) for epoch_data in epoch_data_groups # Number of groups to gather
+            ),
+            pbar_desc="gather",
+        )
+
+        await self.model.delete_checkpoints()
+        await self.model.train(train_groups, config=art.TrainConfig(learning_rate=1e-5))
+
+
+# @art.retry(exceptions=(openai.LengthFinishReasonError,))
+async def rollout(
+    vllm_client: VllmClient,
+    question: str,
+    answer: str,
+    model_config: InternalModelConfig = None,
+    
+) -> art.Trajectory:
+
+    agent = DACAgent(
+        client=vllm_client.client,
+        model=vllm_client.get_inference_name(),
+        # model_system_message=SystemPrompt.Qwen,
+        dac_sys_prompt=DaCSystemPrompt.dac_sys_prompt_v2_3,
+        leaf_sys_prompt=DaCSystemPrompt.dac_sys_prompt_v2_3_leaf,
+        # dac_sys_prompt=prompt,
+        max_depth=1,
+        max_length=4,  # Limit the number of messages in a single chat
+    )
+
+    prompt = "Please answer the following question, write the final answer in the format <answer> final answer </answer>."
+    message = {
+        "role": "user",
+        "content": f"{prompt} \n\"{question}\"",
+    }
+    try:
+        max_tokens = model_config['max_seq_length'] if model_config and 'max_seq_length' in model_config else None
+        trajectory = await agent.chat(message, max_tokens=max_tokens)
+    except Exception as e:
+        print("caught exception generating chat completion")
+        print(e)
+        # global failing_trajectory
+        # failing_trajectory = trajectory
+        return Trajectory(messages_and_choices=[message],reward=0)
+        return e
+
+    content = trajectory.messages()[-1]["content"]
+    agent_answer = extract_text_between_markers(content, "<answer>", "</answer>")
+    # answer = extract_boxed_content(answer)[-1]
+    if len(agent_answer) == 0:
+        trajectory.reward -= 1
+        agent_answer = ""
+    else:
+        agent_answer = agent_answer[-1].strip()  # Get the last answer
+        if agent_answer == answer:
+            trajectory.reward += 1.5  # Reward for correct answer
+            trajectory.metrics["correct_answer"] = 1
+        else:
+            trajectory.reward -= 0.5  # Penalize for incorrect answer
+            trajectory.metrics["correct_answer"] = 0
+    # Add the answer and agent_answer to the trajectory metrics
+    trajectory.metadata["answer"] = answer
+    trajectory.metadata["agent_answer"] = agent_answer
+
+    return trajectory
+
+
+
+
+
+
+
+
+
