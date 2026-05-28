36042ff8ecb62b58bb0d42c035d5a3954a8da6ce
Gilad Freidkin <freidkin.gilad@gmail.com>
2026-05-28 20:02:29 +0300

    updated __init__

--- src/agents/__init__.py
+++ src/agents/__init__.py
@@ -2,10 +2,12 @@ from src.agents.base import BaseAgent
 from src.agents.marker_agent.marker_agent import MarkerAgent
 from src.agents.json_agent.json_agent import JsonAgent
 from src.agents.regex_agent.regex_agent import RegexAgent
+from src.agents.perst_agent.perst_agent import PersistentAgent
 
 __all__ = [
     "BaseAgent",
     "MarkerAgent",
     "JsonAgent",
     "RegexAgent",
+    "PersistentAgent",
 ]
